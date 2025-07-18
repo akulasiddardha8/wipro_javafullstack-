5c1810471a8f70f8d7a556dfc033338b0ffa1839
akulasiddardha8 <akulasiddardha8@gmail.com>
2025-07-18 12:33:49 +0530

    Add files via upload

---
+++ WrapperClasses.java
@@ -0,0 +1,28 @@
+
+public class WrapperClasses {
+public static void main(String[] args) {
+ System.out.println("Integer range:");
+ System.out.println("min: " + Integer.MIN_VALUE);
+ System.out.println("min: " + Integer.MAX_VALUE);
+ System.out.println("Double range:");
+ System.out.println("min: " + Double.MIN_VALUE);
+ System.out.println("min: " + Double.MAX_VALUE);
+ System.out.println("Long range:");
+ System.out.println("min: " + Long.MIN_VALUE);
+ System.out.println("min: " + Long.MAX_VALUE);
+ System.out.println("Short range:");
+ System.out.println("min: " + Short.MIN_VALUE);
+ System.out.println("min: " + Short.MAX_VALUE);
+ System.out.println("Byte range:");
+ System.out.println("min: " + Byte.MIN_VALUE);
+ System.out.println("min: " + Byte.MAX_VALUE);
+ System.out.println("Float range:");
+ System.out.println("min: " + Float.MIN_VALUE);
+ System.out.println("min: " + Float.MAX_VALUE);
+}
+}
+
+
+
+
+
